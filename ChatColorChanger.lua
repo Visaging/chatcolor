f8cee80a734815aade75343e1a6a4dc72ac297a1
Visage <49914133+Visaging@users.noreply.github.com>
2022-10-22 19:09:32 +0530

    Auto Update

--- ChatColorChanger.lua
+++ ChatColorChanger.lua
@@ -9,6 +9,7 @@ require "sampfuncs"
 local inicfg = require "inicfg"
 local se = require "lib.samp.events"
 local effil_res, effil = pcall(require, 'effil')
+local script_path = thisScript().path
 local script_url = "https://raw.githubusercontent.com/Visaging/chatcolor/main/ChatColorChanger.lua"
 local update_url = "https://raw.githubusercontent.com/Visaging/chatcolor/main/ChatColorChanger.txt"
 
